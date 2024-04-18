866fd489530edacc2fd2a1415462fa73eb9c2801
hipostajm <u120_tymgor_waw@technischools.com>
2024-04-18 09:01:21 +0200

    dfghjkl;lkjhgfdewertyh

--- avg_collor.py
+++ avg_collor.py
@@ -5,9 +5,12 @@ down_grade_size = 4
 img = "test.png"
 
 size = Image.open(img).size
-new_size = (int(size[0]/math.sqrt(down_grade_size)),int(size[1]/math.sqrt(down_grade_size)))
+new_size = (
+    int(size[0] / math.sqrt(down_grade_size)),
+    int(size[1] / math.sqrt(down_grade_size)),
+)
 
-new_imige = Image.new(mode='RGB', size=(size[0],size[1]), color='orange')
+new_imige = Image.new(mode="RGB", size=(size[0], size[1]), color="orange")
 
 pixels_new = new_imige.load()
 
@@ -15,15 +18,30 @@ print(new_size)
 
 
 def rgb_of_pixel(img_path: str, x: int, y: int):
-    im = Image.open(img_path).convert('RGB')
+    im = Image.open(img_path).convert("RGB")
     r, g, b = im.getpixel((x, y))
     a = (r, g, b)
     return a
 
-for x in range(size[0]):
-    for y in range(size[1]):
-        a = rgb_of_pixel(img,x,y)
-        # print(f'x: {x}, y: {y}, rgb: {rgb_of_pixel(img,x,y)}')
-        pixels_new[x,y] = a[0],a[1],a[2]
 
-new_imige.save('t.png')
+# for x in range(size[0]):
+#     for y in range(size[1]):
+#         if x % math.sqrt(down_grade_size) == 0 and y % math.sqrt(down_grade_size) == 0:
+#             r_list = []
+#             g_list = []
+#             b_list = []
+#             for new_x in range(int(math.sqrt(down_grade_size) - 1)):
+#                 for new_y in range(int(math.sqrt(down_grade_size) - 1)):
+#                     r_list.append(rgb_of_pixel(img, new_x, new_y)[0])
+#                     g_list.append(rgb_of_pixel(img, new_x, new_y)[1])
+#                     b_list.append(rgb_of_pixel(img, new_x, new_y)[2])
+
+#             r =
+
+for x in range(new_size[0]):
+    for y in range(new_size[1]):
+        print("x: ", x * math.sqrt(down_grade_size), x * math.sqrt(down_grade_size) + 1)
+        print("y: ", y * math.sqrt(down_grade_size), y * math.sqrt(down_grade_size) + 1)
+
+
+new_imige.save("t.png")
